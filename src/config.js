fe57b6dbb12afea9917b32f2712e21d70cf3fc4c
ybitus <92203813+ybitus@users.noreply.github.com>
2023-05-30 08:55:26 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const collectionTicker = 'NFTIM9999';
-export const dAppName = 'My Collection Dapp';
+export const collectionTicker = 'TIRLAART12';
+export const dAppName = 'Picturi Tirla';
 export const  elrondApi = "https://devnet-api.elrond.com"
 export const environment =  "devnet"
